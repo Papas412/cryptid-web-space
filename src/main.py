1e579d26e5554c48d0182f147c7c753bdf784504
Papas412 <gpapas412@gmail.com>
2026-03-21 00:14:26 +0000

    updated main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+def top():
+    return "top here"
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
